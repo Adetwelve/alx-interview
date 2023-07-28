79a1f79ed902f4c84800d385f2770081f1709a3a
Adetwelve <niyero10@gmail.com>
2023-07-27 18:28:31 -0700

    task 0: a UTF-8 Validation script

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+""" A utf-8 validation module """
+
+
+def validUTF8(data):
+    """ a method that determines if a given data
+        set represents a valid UTF-8 encoding
+    """
+    for byte in data:
+        bitchar = format(byte, '08b')
+        if bitchar.startswith('0'):
+            continue
+        elif bitchar.startswith('110'):
+            continue
+        elif bitchar.startswith('1110'):
+            continue
+        elif bitchar.startswith('1110'):
+            continue
+        else:
+            return False
+    return True
